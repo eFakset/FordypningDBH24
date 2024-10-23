b67c728a78c0b25143e0d3fb82095ed2b3b7bba3
eFakset <ellen.fakset@sfjbb.net>
2024-10-23 14:47:02 +0200

    Initial

--- Scripts/Uttrekk faktatabeller.sql
+++
@@ -1,8 +0,0 @@
-select concat('insert into handel values(', handel_nr, ', ''', tidspunkt, ''', ', butikk_nr, ', ', kasse_nr, ', ', kunde_nr, ', ', handel_bel, ');')
-from handel;
-
-select concat('insert into varelinje values(', handel_nr, ',', varelinje_nr, ',', vare_nr, ',', enhet_ant, ',', enhetpris_bel, ',', varelinje_bel, ');')
-from varelinje;
-
-select concat('insert into bonus values(', handel_nr, ',', varegruppe_nr, ',', bonus_bel, ');')
-from bonus;
